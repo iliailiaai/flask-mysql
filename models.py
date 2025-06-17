95cd68f6668045a1c8f773b9a0018406f14cc02d
iliailiaai <131950015+iliailiaai@users.noreply.github.com>
2025-06-17 17:25:56 +0300

    Update models.py

--- models.py
+++ models.py
@@ -9,23 +9,4 @@ class Item(db.Model):
 
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id_email = db.Column(db.String, primary_key=True)
-    user_data = db.relationship("UserData", back_populates="user", uselist=False, cascade="all, delete-orphan")
 
-
-class UserData(db.Model):
-    __tablename__ = 'user_data'
-    id = db.Column(db.Integer, primary_key=True)
-    user_email = db.Column(db.String, db.ForeignKey('users.id_email'), nullable=False, unique=True)
-
-    purpose = db.Column(db.String(100))
-    gender = db.Column(db.String(20))
-    level = db.Column(db.String(50))
-    frequency = db.Column(db.Integer)
-    trauma = db.Column(db.String(255))
-    muscles = db.Column(db.String(255))
-    age = db.Column(db.Integer)
-
-    user = db.relationship("User", back_populates="user_data")
